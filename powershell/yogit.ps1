614f2231fbe79b4a2b443f1a8f500a2a8447e6ed
tizee <33030965+tizee@users.noreply.github.com>
2025-02-06 00:15:44 +0800

    feat(powershell): Add `ygcount` function to count commits 🎉📊
    - Added a new function `ygcount` to `yogit.ps1` that uses `git rev-list --count HEAD` to count the number of commits in the current branch. 📈
    - This will help users quickly get the commit count without leaving their terminal. 🚀
    
    Now you can flex your commit count like a pro! 💪😎

--- powershell/yogit.ps1
+++ powershell/yogit.ps1
@@ -10,37 +10,41 @@
 
 # Function to get the current Git branch
 function __yogit_get_current_branch {
-    return git branch --show-current
+  return git branch --show-current
 }
 
 # Function equivalent to ygpush alias in zsh
 function ygpush {
-    $currentBranch = __yogit_get_current_branch
+  $currentBranch = __yogit_get_current_branch
     git push origin $currentBranch
 }
 
 # Function equivalent to ygpull alias in zsh
 function ygpull {
-    $currentBranch = __yogit_get_current_branch
+  $currentBranch = __yogit_get_current_branch
     git pull origin $currentBranch
 }
 
 function ygsc($repo) {
-	git clone $repo --depth=1
+  git clone $repo --depth=1
 }
 
 function ygc() {
-	git commit
+  git commit
 }
 
 function ygurl {
-    param(
-        [string]$remote_name = "origin"
-    )
+  param(
+      [string]$remote_name = "origin"
+      )
     $gitUrl = git config --get "remote.$remote_name.url"
     if ($gitUrl -match '^[^@]*@([^:\/]*)([:\/])') {
-        $gitUrl = $gitUrl -replace "^[^@]*@([^:\/]*)([:\/])", "https://`$1/"
+      $gitUrl = $gitUrl -replace "^[^@]*@([^:\/]*)([:\/])", "https://`$1/"
     }
-    $gitUrl = $gitUrl -replace '\.git$'
+  $gitUrl = $gitUrl -replace '\.git$'
     echo $gitUrl
+}
+
+function ygcount() {
+  git rev-list --count HEAD
 }
