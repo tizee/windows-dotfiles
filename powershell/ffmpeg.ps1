1837c0abf097b94976cf125f50b1b0e5fe5818f5
tizee <33030965+tizee@users.noreply.github.com>
2025-02-08 17:43:23 +0800

    feat(powershell): 🎥✨ Add `Download-Ytb` function for YouTube video downloading
    - Introduced a new function `Download-Ytb` to download YouTube videos or playlists in 1080p with the best audio quality using `yt-dlp`. 🚀
    - Function checks for `yt-dlp` installation and gracefully handles its absence. 🛠️
    - Supports both single video and playlist downloads with a `-Playlist` switch. 📚
    - Added detailed help documentation with examples for clarity. 📖
    
    Now you can grab those cat videos in glorious 1080p! 🐱🎬

--- powershell/ffmpeg.ps1
+++ powershell/ffmpeg.ps1
@@ -1,3 +1,47 @@
+function Download-Ytb {
+  <#
+  .SYNOPSIS
+  Downloads YouTube videos in 1080p with best audio quality using yt-dlp.
+
+  .DESCRIPTION
+  This function checks if yt-dlp is installed, then downloads a YouTube video or playlist
+  in 1080p resolution with the best available audio quality.
+
+  .PARAMETER VideoUrl
+  The URL of the YouTube video or playlist.
+
+  .PARAMETER Playlist
+  A switch parameter indicating whether the URL is a playlist. If present, the entire playlist will be downloaded.
+
+  .EXAMPLE
+  Download-Ytb -VideoUrl "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  Downloads the single video with the specified URL.
+
+  .EXAMPLE
+  Download-Ytb -VideoUrl "https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=PLE0hg-LdSfycrpTtMImPSqFLle4yYNzWD" -Playlist
+  Downloads the entire playlist with the specified URL.
+  #>
+  param (
+    [string]$VideoUrl,  # The URL of the YouTube video or playlist
+    [switch]$Playlist   # Flag to indicate if the URL is a playlist
+  )
+
+  # Check if yt-dlp is installed
+  if (Get-Command yt-dlp -ErrorAction SilentlyContinue) {
+    # Determine whether to include the playlist argument
+    $playlist_arg = if ($Playlist) { "--yes-playlist" } else { "" }
+
+    # Construct the yt-dlp command
+    $yt_dlp_command = "yt-dlp --no-mtime $playlist_arg --audio-format best --format `"bestvideo[height=1080]+bestaudio/best[height<=1080]/best`" --merge-output-format mp4 $VideoUrl"
+
+    # Execute the yt-dlp command
+    Invoke-Expression $yt_dlp_command
+  } else {
+    # Display an error message if yt-dlp is not installed
+    Write-Error "yt-dlp is not installed. Please install it before using this function."
+  }
+}
+
 function Fix-YouTubeVideo {
     [CmdletBinding()]
     param (
